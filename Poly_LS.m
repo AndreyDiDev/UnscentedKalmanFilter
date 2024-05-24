213c5ce8e57979b113d45dd74373384f8e7ba6c7
Andrey D <andinfbi@gmail.com>
2024-05-23 20:00:51 -0600

    testing diff methods

--- Poly_LS.m
+++ Poly_LS.m
@@ -2,12 +2,36 @@ close all
 
 T = readtable('C:\Users\Andrey\Documents\UKFRepo\UnscentedKalmanFilter\sims.csv');
 
+time = T.Time_s_;
+z = T.PositionZ_m_;
+
 [p,~,mu] = polyfit(T.Time_s_, T.PositionZ_m_, 190);
 
 f = polyval(p,T.Time_s_,[],mu);
+
 hold on
 plot(T.Time_s_,f)
+% hold off
+
+population3 = fit(time,z,'poly3','Normalize','on');
+population4 = fit(time,z,'poly4','Normalize','on');
+population5 = fit(time,z,'poly5','Normalize','on');
+population6 = fit(time,z,'poly6','Normalize','on');
+
+populationExp = fit(time,z,'exp1');
+
+% new = fit(time,z);
+
+hold on
+plot(population3,'b');
+plot(population4,'g');
+plot(population5,'m');
+plot(population6,'b--');
+plot(populationExp,'r--');
+% plot(new, 'r');
 hold off
+legend('time v z','poly2','poly3','poly4','poly5','poly6','exp1');
+xlim([0 350]);
 
 % x = [T.Time_s_]';
 % y = [T.PositionZ_m_]';
---
+++ test.m
@@ -0,0 +1,19 @@
+close all
+
+T = readtable('C:\Users\Andrey\Documents\UKFRepo\UnscentedKalmanFilter\sims.csv');
+
+time = T.Time_s_;
+z = T.PositionZ_m_;
+
+hold on
+plot(time,z, 'g-');
+hold off
+
+[p,~,mu] = polyfit(T.Time_s_, T.PositionZ_m_, 200);
+
+f = polyval(p,T.Time_s_,[],mu);
+
+ylim([0 3000])
+
+hold on
+plot(T.Time_s_,f)
