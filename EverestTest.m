b0a5577ccd53f130997a51459531fab44399450e
Andrey D <andinfbi@gmail.com>
2024-07-20 21:02:15 -0600

    Update EverestTest.m

--- EverestTest.m
+++ EverestTest.m
@@ -4,6 +4,14 @@ clear;
 alt_Data = readtable('C:\Users\Andrey\Downloads\Altimeter.csv');
 filtered_Data = readtable('C:\Users\Andrey\Documents\EverestRepo\Apogee-Detection-Everest\EverestLibrary\everest3.txt');
 baro_Data = readtable('C:\Users\Andrey\Downloads\baro_3.csv');
+sims = readtable('C:\Users\Andrey\Documents\UKFRepo\UnscentedKalmanFilter\sims.csv');
+simmies = readtable('C:\Users\Andrey\Downloads\simmies.csv');
+
+sims_time = sims.Time_s_;
+sims_alt = sims.PositionZ_m_ + 1013;
+
+simmies_time = simmies.Time_s_;
+simmies_alt = simmies.PositionZ_m_;
 
 % altimeter
 time = alt_Data.time;
@@ -20,7 +28,9 @@ h = atmospalt(baro);
 %Rsquared = my_Rsquared_coeff(true,filtered); % correlation coefficient
 subplot(1,1,1)
 title('Filtered vs True');
-plot(time,true,'b-',filtered_time,filtered,'r-', filtered_time, h, 'c')
+plot(simmies_time,simmies_alt,'b-',filtered_time,filtered,'r-', filtered_time, h, 'c', time, true, 'o')
+
+legend("True")
 
 %figure(1);
 %plot(time, true, 'c');
