2a4f08a6a1d3c35fe8d40a4a722cc928e4b0acb6
Andrey D <andinfbi@gmail.com>
2024-07-22 13:18:26 -0600

    Update Universal.cpp

--- Universal.cpp
+++ Universal.cpp
@@ -70,22 +70,61 @@ void prediction(){
 
     // initialize scenario to default model-A(vg)
     // given the Madgwick acc, velo, alt
-    // interpolate between 2 models 
+    // interpolate between 2 models
+
 
     // predict scenario t+1 based on interpolated values
 
 }
 
 // Function to interpolate between two nearest scenarios
-float interpolateScenarios(float x) {
-    float y1 = scenarios[0].evaluate(x);
-    float y2 = scenarios[1].evaluate(x);
+float interpolateScenarios(VectorXf &X_in, std::vector<Scenario> &scenarios) {
+
+    // Find the nearest 2 scenarios to the current state
+    auto predicted_acc = findNearestScenarios(scenarios, X_in(0), X_in(1), 'a');
+    auto predicted_velo = findNearestScenarios(scenarios, X_in(0), X_in(1), 'v');
+    auto predicted_alt = findNearestScenarios(scenarios, X_in(0), X_in(1), 'h');
+
+    // Interpolate between the two scenarios
+    float interpolated_acc = interpolate(X_in(1), predicted_acc.acceleration, predicted_velo.acceleration);
 
     // Simple average for interpolation
     return (y1 + y2) / 2;
 }
 
 
+/**
+ * Given a list of scenarios, find the nearest 2 scenarios to a target value
+ */
+vector<Scenario> findNearestScenarios(const std::vector<Scenario>& scenarios, float x, float targetValue, char measure) {
+    std::vector<std::pair<float, Scenario>> distances;
+
+    for (const auto& scenario : scenarios) {
+        float value;
+        switch (measure) {
+            case 'a': // Acceleration
+                value = scenario.evaluateAcceleration(x);
+                break;
+            case 'v': // Velocity
+                value = scenario.evaluateVelocity(x);
+                break;
+            case 'h': // Altitude
+                value = scenario.evaluateAltitude(x);
+                break;
+            default:
+                throw std::invalid_argument("Invalid measure type");
+        }
+
+        float distance = std::abs(value - targetValue);
+        distances.emplace_back(distance, scenario);
+    }
+
+    std::sort(distances.begin(), distances.end(), [](const auto& a, const auto& b) {
+        return a.first < b.first;
+    });
+
+    return distances.front().second;
+}
 
 // R = control noise 
 // Q = measurement noise 
