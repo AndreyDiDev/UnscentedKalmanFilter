8397adee1e3193685ed30e19da3a3f81df7d1d6c
Andrey D <andinfbi@gmail.com>
2024-08-02 12:23:02 -0600

    trying to get it to work

--- Universal.cpp
+++ Universal.cpp
@@ -1,5 +1,5 @@
 #include "universal.hpp"
-#include "C:\\Users\\Andrey\\Downloads\\eigen-3.4.0\\eigen-3.4.0\\Eigen\\Dense"
+#include "C:\\Users\\andin\\Downloads\\eigen-3.4.0\\Eigen\\Dense"
 // #include "everestTaskHPP.hpp"
 
 #ifndef UNIVERSAL_CPP
@@ -10,11 +10,11 @@
 #define REFRESH_RATE 20
 
 // Constants for the UKF
-#define N 2
-#define dim 2
-#define alpha 0.1
-#define beta 2
-#define k 3 - dim // 3 dimensions
+// #define N 2
+// #define dim 2
+// #define alpha 0.1
+// #define beta 2
+// #define k 3 - dim // 3 dimensions
 
 using namespace Eigen;
 
@@ -27,28 +27,28 @@ using namespace Eigen;
 void Universal::init(MatrixXf &X0, MatrixXf &P0, VectorXf &Z_in){
     // Input: Estimate Uncertainty -> system state
     // Initial Guess
-    this->X0 = X0;
-    this->P = P0;
-    this->Z = Z_in;
+    // this->X0 = X0;
+    // this->P = P0;
+    // this->Z = Z_in;
 
-    // F is for state to next state transition
-    // P0 = initial guess of state covariance matrix
-    // Q = process noise covariance matrix
-    // R = measurement noise covariance matrix
+    // // // F is for state to next state transition
+    // // // P0 = initial guess of state covariance matrix
+    // // // Q = process noise covariance matrix
+    // // // R = measurement noise covariance matrix
 
-    // Weights for sigma points
-    float lambda = std::pow(alpha, 2) * (dim + k) - dim;
-    float w0_m = lambda / (dim + k);                                         // weight for first sPoint when cal mean
-    float w0_c = lambda/(3 + lambda) + (1 - std::pow(alpha, 2) + beta);    // weight for first sPoint when cal covar
+    // // Weights for sigma points
+    // float lambda = std::pow(alpha, 2) * (dim + k) - dim;
+    // float w0_m = lambda / (dim + k);                                         // weight for first sPoint when cal mean
+    // float w0_c = lambda/(3 + lambda) + (1 - std::pow(alpha, 2) + beta);    // weight for first sPoint when cal covar
 
-    MatrixXf Weights(2 * dim + 1);
-    Weights.setZero((2 * 3) + 1);   // 2N + 1
-    Weights.setConstant(2 * dim + 1, w0_c);
-    Weights(0) = w0_m;
+    // MatrixXf Weights(2 * dim + 1);
+    // Weights.setZero((2 * 3) + 1);   // 2N + 1
+    // Weights.setConstant(2 * dim + 1, w0_c);
+    // Weights(0) = w0_m;
 
-    this->WeightsUKF = Weights;
+    // this->WeightsUKF = Weights;
 
-    std::cout << "Weights: " << Weights << std::endl;
+    // std::cout << "Weights: " << Weights << std::endl;
 
     // X0 = [acceleration, velocity, altitude]
     // X0 << this->getFAccel(), this->getFVelo(), this->getFAlt();
@@ -121,68 +121,68 @@ void Universal::prediction(){
 
 }
 
-MatrixXf calculateSigmaPoints(MatrixXf &X0, MatrixXf &P0, MatrixXf &Q, MatrixXf &projectError, MatrixXf &Weights) {
-    // MatrixXf sigmaPoints(2,2);
-    // Calculate the square root of (N+k) * P0 using Cholesky decomposition
-    float lambda = std::pow(alpha, 2) * (N + k) - N;
-    std::cout << "lambda = " << lambda << std::endl;
+// MatrixXf calculateSigmaPoints(MatrixXf &X0, MatrixXf &P0, MatrixXf &Q, MatrixXf &projectError, MatrixXf &Weights) {
+//     // MatrixXf sigmaPoints(2,2);
+//     // Calculate the square root of (N+k) * P0 using Cholesky decomposition
+//     // float lambda = std::pow(alpha, 2) * (N + k) - N;
+//     // std::cout << "lambda = " << lambda << std::endl;
 
-    // MatrixXf sqrtP0 = (N + k) * P0;
-    // MatrixXf sqrtP0(2,2);
-    // sqrtP0 << 0, 0,
-    //         0, 0;
+//     // MatrixXf sqrtP0 = (N + k) * P0;
+//     // MatrixXf sqrtP0(2,2);
+//     // sqrtP0 << 0, 0,
+//     //         0, 0;
 
-    MatrixXf P(2,2);
-    P << 5, 0, 
-        0, 5;
+//     MatrixXf P(2,2);
+//     P << 5, 0, 
+//         0, 5;
 
-    MatrixXf L( ((dim + k) *P).llt().matrixL());
-    std::cout << L.col(0) << std::endl;
+//     MatrixXf L( ((dim + k) *P).llt().matrixL());
+//     std::cout << L.col(0) << std::endl;
 
 
-    std::cout << "calc sPts" << std::endl;
-    // std::cout << "SqrtP0: " << sqrtP0 << std::endl;
+//     std::cout << "calc sPts" << std::endl;
+//     // std::cout << "SqrtP0: " << sqrtP0 << std::endl;
 
-    // LDLT<MatrixXf> lltOfP0(P0); // Perform Cholesky decomposition
-    // MatrixXf L = lltOfP0.matrixL(); // Retrieve the lower triangular matrix
+//     // LDLT<MatrixXf> lltOfP0(P0); // Perform Cholesky decomposition
+//     // MatrixXf L = lltOfP0.matrixL(); // Retrieve the lower triangular matrix
 
-    // // Initialize sigma points matrix
-    MatrixXf sigmaPoints(dim, (2 * N) + 1);
-    sigmaPoints.setZero();
+//     // // Initialize sigma points matrix
+//     MatrixXf sigmaPoints(dim, (2 * N) + 1);
+//     sigmaPoints.setZero();
 
-    // // Set the first sigma point
-    sigmaPoints.col(0) = X0;
+//     // // Set the first sigma point
+//     sigmaPoints.col(0) = X0;
 
-    // Set the remaining sigma points
-    for (int i = 1; i < dim + 1; i++) {
-        sigmaPoints.row(i + 1) = X0 + L.col(i);
-        sigmaPoints.row(i + 1 + N) = X0 - L.col(i);
-    }
+//     // Set the remaining sigma points
+//     for (int i = 1; i < dim + 1; i++) {
+//         // sigmaPoints.row(i + 1) = X0 + L.col(i);
+//         // sigmaPoints.row(i + 1 + N) = X0 - L.col(i);
+//     }
 
-    for(int j = dim + 1; j < (2 * N) + 1; j++){
-        sigmaPoints.col(j) = X0 + L.col(j - dim - 1);
-    }
+//     for(int j = dim + 1; j < (2 * N) + 1; j++){
+//         // sigmaPoints.col(j) = X0 + L.col(j - dim - 1);
+//     }
 
-    // propagate sigma points through the dynamic model
-    for(int i = 0; i < (2 * N) + 1; i++){
-        // sigmaPoints.col(i) = dynamicModel(sigmaPoints.col(i));
-    }
+//     // propagate sigma points through the dynamic model
+//     for(int i = 0; i < (2 * N) + 1; i++){
+//         // sigmaPoints.col(i) = dynamicModel(sigmaPoints.col(i));
+//     }
 
-    // calculate the mean and covariance of the sigma points
-    MatrixXf Xprediction;
-    Xprediction = sigmaPoints * Weights;
+//     // calculate the mean and covariance of the sigma points
+//     MatrixXf Xprediction;
+//     // Xprediction = sigmaPoints * Weights;
 
-    // MatrixXf projectError;
-    projectError.setZero(dim, (2 * N) + 1);
-    for(int j = 0; j < dim; j++){
-        projectError.row(j) = (sigmaPoints.row(j).array() - Xprediction.row(j).value()).matrix();
-    }
+//     // MatrixXf projectError;
+//     // projectError.setZero(dim, (2 * N) + 1);
+//     for(int j = 0; j < dim; j++){
+//         // projectError.row(j) = (sigmaPoints.row(j).array() - Xprediction.row(j).value()).matrix();
+//     }
 
-    // assuming non linear dynamics
-    MatrixXf Pprediction = projectError * Weights.asDiagonal() * projectError.transpose() + Q;
+//     // assuming non linear dynamics
+//     // MatrixXf Pprediction = projectError * Weights.asDiagonal() * projectError.transpose() + Q;
 
-    return sigmaPoints;
-}
+//     return sigmaPoints;
+// }
 
 // Function to interpolate between two nearest scenarios
 float Universal::interpolateScenarios(VectorXf &X_in, std::vector<Scenario> &scenarios) {
@@ -358,45 +358,231 @@ MatrixXf dynamicModel(MatrixXf &X){
     return Xprediction;
 }
 
+// Aiden
+Universal::Universal(int dim_x, int dim_z)
+{
+    this->dim_x = dim_x;
+    this->dim_z = dim_z;
+    k = 3 - dim_x;
+}
+
+void Universal::init(VectorXf &X0, MatrixXf &P0, VectorXf &Z_in, MatrixXf &R_in)
+{
+    R = &R_in;
+    Z = &Z_in;
+
+    linearDynamics = false;
+
+    // Calculate weights
+    w0 = k / (dim_x + k);
+    w1 = 1 / (2 * (dim_x + k));
+    W.setZero((2 * dim_x) + 1);
+    W1.setConstant(2 * dim_x, w1);
+    W << w0, W1;
+
+    // initalize and predict next state
+    X = X0;
+    P = P0;
+    uTransform();
+}
+
+void Universal::init(VectorXf &X0, MatrixXf &P0, VectorXf &Z_in, MatrixXf &R_in, MatrixXf &H_in)
+{
+    H = &H_in;
+    R = &R_in;
+    Z = &Z_in;
+
+    linearDynamics = false;
+
+    // Calculate weights
+    k = 3 - dim_x;
+    w0 = k / (dim_x + k);
+    w1 = 1 / (2 * (dim_x + k));
+    W.setZero((2 * dim_x) + 1);
+    W1.setConstant(2 * dim_x, w1);
+    W << w0, W1;
+
+    // initalize and predict next state
+    X = X0;
+    P = P0;
+    uTransform();
+}
+
+void Universal::init(VectorXf &X0, MatrixXf &P0, VectorXf &Z_in, MatrixXf &R_in, MatrixXf &F_in, MatrixXf &Qa_in)
+{
+    F = &F_in;
+    R = &R_in;
+    Z = &Z_in;
+
+    linearDynamics = true;
+
+    Q = *F * Qa_in * F->transpose();
+
+    // Calculate weights
+    k = 3 - dim_x;
+    w0 = k / (dim_x + k);
+    w1 = 1 / (2 * (dim_x + k));
+    W.setZero((2 * dim_x) + 1);
+    W1.setConstant(2 * dim_x, w1);
+    W << w0, W1;
+
+    // initalize and predict next state
+    X = X0;
+    P = P0;
+    uTransform();
+}
+
+void Universal::init(VectorXf &X0, MatrixXf &P0, VectorXf &Z_in, MatrixXf &R_in, MatrixXf &H_in, MatrixXf &F_in, MatrixXf &Qa_in)
+{
+    H = &H_in;
+    F = &F_in;
+    R = &R_in;
+    Z = &Z_in;
+
+    linearDynamics = true;
+
+    Q = *F * Qa_in * F->transpose();
+
+    // Calculate weights
+    k = 3 - dim_x;
+    w0 = k / (dim_x + k);
+    w1 = 1 / (2 * (dim_x + k));
+    W.setZero((2 * dim_x) + 1);
+    W1.setConstant(2 * dim_x, w1);
+    W << w0, W1;
+
+    // initalize and predict next state
+    X = X0;
+    P = P0;
+    uTransform();
+}
+
+void Universal::update()
+{
+    sMeasurements.setZero(dim_z, (2 * dim_x) + 1);
+    for (int i = 0; i < (2 * dim_x) + 1; i++)
+    {
+        sMeasurements.col(i) = observe(sPoints.col(i));
+    }
+    Zpred = sMeasurements * W;
+    
+    measurementProjError.setZero(dim_z, (2 * dim_x) + 1);
+    for (int i = 0; i < dim_z; i++)
+    {
+        measurementProjError.row(i) = (sMeasurements.row(i).array() - Zpred.row(i).value()).matrix();
+    }
+
+    WeightedZvariance = (measurementProjError * W.asDiagonal() * measurementProjError.transpose()) + *R; // Compute measurement variance matrix
+    XZCovariance = projError * W.asDiagonal() * measurementProjError.transpose();                        // Compute state-measurement cross covariance matrix
+    K = XZCovariance * WeightedZvariance.inverse();                                                      // Compute Kalman gain
+    X = Xpred + K * (*Z - Zpred);                                                                        // update state prediction
+    P = Ppred - (K * WeightedZvariance * K.transpose());                                                 // update Covariance
+    uTransform();                                                                                        // make next predictions
+}
+
+MatrixXf Universal::observe(MatrixXf sigmaX)
+{
+    // return *H * sigmaX; //linear observation equation default.
+    VectorXf Zout(2);
+    Zout << sqrt(pow(sigmaX(0), 2) + pow(sigmaX(3), 2)),
+            atan(sigmaX(3)/sigmaX(0));
+    return Zout;
+}
+
+MatrixXf Universal::predict(MatrixXf sigmaX)
+{
+    return *F * sigmaX; // linear system dynamics default.
+}
+
+void Universal::uTransform()
+{
+    // Generate sigma points
+    sPoints.setZero(dim_x, (2 * dim_x) + 1);
+    sPoints.col(0) = X;
+    sigma = ((dim_x + k) * P).llt().matrixL();
+    for (int i = 1; i < dim_x + 1; i++)
+    {
+        sPoints.col(i) = X + sigma.col(i - 1);
+    }
+    for (int i = dim_x + 1; i < (2 * dim_x) + 1; i++)
+    {
+        sPoints.col(i) = X - sigma.col(i - dim_x - 1);
+    }
+
+    // Propigate points
+    for (int i = 0; i < (2 * dim_x) + 1; i++)
+    {
+        sPoints.col(i) = predict(sPoints.col(i));
+    }
+
+    // Calculate weighted mean for points
+    Xpred = sPoints * W;
+    projError.setZero(dim_x, (2 * dim_x) + 1);
+    for (int i = 0; i < dim_x; i++)
+    {
+        projError.row(i) = (sPoints.row(i).array() - Xpred.row(i).value()).matrix();
+    }
+    if (linearDynamics == true)
+    {
+        Ppred = projError * W.asDiagonal() * projError.transpose() + Q;
+    }
+    else
+    {
+        Ppred = projError * W.asDiagonal() * projError.transpose();
+    }
+}
+
+
 int main(){
-    // Initialize the state vector
-    // setStateVector(Everest::filteredAcc, Everest::filteredVelo, Everest::filteredAlt);
-    // setStateVector(0, 0, 1000);
+    VectorXf X0u(6);
+    X0u << 400, 0, 0, -300, 0, 0;
 
-    // // Initialize the covariance matrix
-    // MatrixXf P0(3, 3);
-    // P0 << 3, 0, 0,
-    //       0, 3, 0,
-    //       0, 0, 3;
+    MatrixXf P0u(6, 6);
+    P0u << 500, 0, 0, 0, 0, 0,
+        0, 500, 0, 0, 0, 0,
+        0, 0, 500, 0, 0, 0,
+        0, 0, 0, 500, 0, 0,
+        0, 0, 0, 0, 500, 0,
+        0, 0, 0, 0, 0, 500;
 
-    // // Initialize the UKF
-    // init(P0, Z_in, R_in);
+    VectorXf Zu(2);
 
-    // // Update the UKF
-    // update();
+    MatrixXf Ru(2, 2);
+    Ru << 25, 0,
+        0, 0.00007569;
 
-    // // Predict the next values
-    // prediction();
+    MatrixXf Fu(6, 6);
+    Fu << 1, 1, 0.5, 0, 0, 0,
+        0, 1, 1, 0, 0, 0,
+        0, 0, 1, 0, 0, 0,
+        0, 0, 0, 1, 1, 0.5,
+        0, 0, 0, 0, 1, 1,
+        0, 0, 0, 0, 0, 1;
 
-    MatrixXf X0(2, 1);
-    X0 << 0.0873,
-          0;
+    MatrixXf Qau(6, 6);
+    Qau << 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0,
+        0, 0, 0.04, 0, 0, 0,
+        0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0.04;
 
-    MatrixXf P0(2, 2);
-    P0 << 5, 0,
-          0, 5;
+    double posu[2][35] = {{502.55, 477.34, 457.21, 442.94, 427.27, 406.05, 400.73, 377.32, 360.27, 345.93, 333.34, 328.07, 315.48,
+                          301.41, 302.87, 304.25, 294.46, 294.29, 299.38, 299.37, 300.68, 304.1, 301.96, 300.3, 301.9, 296.7, 297.07,
+                          295.29, 296.31, 300.62, 292.3, 298.11, 298.07, 298.92, 298.04},
+                         {-0.9316, -0.8977, -0.8512, -0.8114, -0.7853, -0.7392, -0.7052, -0.6478, -0.59, -0.5183, -0.4698, -0.3952, -0.3026,
+                          -0.2445, -0.1626, -0.0937, 0.0085, 0.0856, 0.1675, 0.2467, 0.329, 0.4149, 0.504, 0.5934, 0.667, 0.7537, 0.8354,
+                          0.9195, 1.0039, 1.0923, 1.1546, 1.2564, 1.3274, 1.409, 1.5011}};
 
-    VectorXf Z_in;
-    Z_in << 0;
-
-    std::cout << "X0:\n" << X0 << std::endl;
-
-    MatrixXf sigmaPoints = calculateSigmaPoints(X0, P0, P0, P0, P0);
-
-    std::cout << "Sigma Points:\n" << sigmaPoints << std::endl;
-
-    Universal uni = Universal();
-    uni.init(X0, P0, Z_in);
+    Universal car(6, 2);
+    car.init(X0u, P0u, Zu, Ru, Fu, Qau);
+    for (int l = 0; l < 35; l++)
+    {
+        Zu << posu[0][k], posu[1][k];
+        car.update();
+    }
+    std::cout << car.getState() << std::endl;
+    std::cout << car.getCov() <<   std::endl;
 
 
     return 0;
--- universal.hpp
+++ universal.hpp
@@ -1,12 +1,14 @@
 #ifndef UNIVERSAL_HPP
 #define UNIVERSAL_HPP
 
-#include "C:\\Users\\Andrey\\Downloads\\eigen-3.4.0\\eigen-3.4.0\\Eigen\\Cholesky"
-#include "C:\\Users\\Andrey\\Downloads\\eigen-3.4.0\\eigen-3.4.0\\Eigen\\Dense"
+#include "C:\\Users\\andin\\Downloads\\eigen-3.4.0\\Eigen\\Cholesky"
+#include "C:\\Users\\andin\\Downloads\\eigen-3.4.0\\Eigen\\Dense"
 #include <cmath>
 #include <string>
 #include <vector>
 #include <iostream>
+
+#include "Kalman.hpp"
 
 
 bool isBeforeApogeeBool = false;
@@ -88,7 +90,7 @@ struct Scenario {
 };
 
 
-class Universal{
+class Universal : public Kalman{
     public:
         void init(MatrixXf &X0, MatrixXf &P0, VectorXf &Z_in);
 
@@ -125,6 +127,42 @@ class Universal{
         std::vector<std::pair<float, Scenario>> findNearestScenarios(const std::vector<Scenario>& scenarios, float time, float targetValue, char measure);
 
         float interpolateScenarios(VectorXf &X_in, std::vector<Scenario> &scenarios);
+    
+        // Aiden
+        /* Initalize the Kalman filter
+        
+        F_in -> State transition matrix (dim_x X dim_x) (For linear system dynamics only)
+        H_in -> Observation matrix (dim_z X dim_x) (For linear observation equations only)
+        Qa_in -> Process noise covariance initializer, see below (dim_x X dim_x) (For linear system dynamics only)
+        Z_in -> Measurement vector. Update passed vector with new values each time step. (dim_z)
+        R_in -> Measurement Covariance matrix. Update passed matrix with new values each time step. (dim_z X dim_z)
+        X0 -> inital state vector. (dim_x)
+        P0 -> inital estimate covariance (dim_x X dim_x)
+
+        Note: Qa should be initialized as s^2[M], where M has a 1 on the main diagonal corresponding to each
+        * state variable associated with s^2. For instance, state vector [x,y] where only y is affected by variance has Qa =
+        * [0,  0]
+        * [0,s^2]
+        */
+        virtual void init(VectorXf &X0, MatrixXf &P0, VectorXf &Z_in, MatrixXf &R_in);
+
+        virtual void init(VectorXf &X0, MatrixXf &P0, VectorXf &Z_in, MatrixXf &R_in, MatrixXf &H_in);
+
+        virtual void init(VectorXf &X0, MatrixXf &P0, VectorXf &Z_in, MatrixXf &R_in, MatrixXf &F_in, MatrixXf &Qa_in);
+
+        virtual void init(VectorXf &X0, MatrixXf &P0, VectorXf &Z_in, MatrixXf &R_in, MatrixXf &H_in, MatrixXf &F_in, MatrixXf &Qa_in);
+
+        /*Update the filter with new measurements
+        Note that this reads the variables Z and R passed to init() for new data.*/
+        virtual void update();
+
+        /*
+        * Constructor
+        * dim_x: number of state variables (size of state vector X)
+        * dim_z: number of measurement inputs (size of measurement vector Z)
+        */
+        Universal(int dim_x, int dim_z);
+    
     private:
         float Uaccel;
         float Ualt;
@@ -161,6 +199,31 @@ class Universal{
         kinematics Kinematics;
 
         kinematics* getKinematics();
+
+        /*Takes state sigma points and transforms them to the measurement space (Ex: returning a Measurement sigma point matrix). Must be overwritten with 
+        non-linear observation equations. Uses H matrix passed to init() function by default (useful if observation equations are linear).*/
+        MatrixXf observe(MatrixXf sigmaX);
+
+        /*Takes state vector X and calculates state based off of previous state, to be stored in Xpred.  Must be overwritten with
+        non-linear system dynamics. Uses F matrix passed to init() by default (useful if system dynamics are linear) 
+        X will be a dim_x X 1 matrix, output must also be a dim_x X 1 matrix.*/
+        MatrixXf predict(MatrixXf sigmaX);
+
+        /*Function to perform the unscented transform*/
+        void uTransform();
+
+        float k; // Tuning parameter for linearization.
+        MatrixXf sPoints; //to be assigned an array of sigma points
+        MatrixXf sigma; //Decomposed matrix used for finding sigma points
+        VectorXf W; //weight matrix / vector for sigma points
+        VectorXf W1; //Matrix for filling W
+        float w0, w1; //weights for W matrix
+        MatrixXf projError; //Matrix for calculating projected covariance
+        MatrixXf sMeasurements; //Matrix of measurement predictions from Unscented transform
+        VectorXf Zpred; // expected measurement vector - needs to be explicitly defined for UKF
+        MatrixXf WeightedZvariance; //weighted measurement variance matrix
+        MatrixXf XZCovariance; // state-measuremnt cross covariance matrix
+        MatrixXf measurementProjError; //Matrix for calculating weighted measurement variance matrix
 
 
 };
